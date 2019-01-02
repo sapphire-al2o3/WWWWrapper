11e9a77e1ab1ed48e4223ccb69444645a0e2fc5f
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-02 14:51:53 +0900

    fix indent

--- Assets/WWWWrapper.cs
+++ Assets/WWWWrapper.cs
@@ -20,54 +20,54 @@ public class WWWWrapper : CustomYieldInstruction, IDisposable
         "Cannot resolve destination host"
     };
 
-	float delayStartTime = 0.0f;
+    float delayStartTime = 0.0f;
 
 
     ErrorPlatform errorPlatform = ErrorPlatform.Android;
 
-	public bool forceError
-	{
-		set; get;
-	}
+    public bool forceError
+    {
+        set; get;
+    }
 
-	public float delay
-	{
-		set; get;
-	}
+    public float delay
+    {
+        set; get;
+    }
 
-	public WWW www
-	{
-		private set; get;
-	}
+    public WWW www
+    {
+        private set; get;
+    }
 
-	public WWWWrapper(string url)
-	{
-		www = new WWW(url);
-		delayStartTime = Time.realtimeSinceStartup;
-	}
+    public WWWWrapper(string url)
+    {
+        www = new WWW(url);
+        delayStartTime = Time.realtimeSinceStartup;
+    }
 
-	public WWWWrapper(string url, WWWForm form)
-	{
-		www = new WWW(url, form);
-		delayStartTime = Time.realtimeSinceStartup;
-	}
+    public WWWWrapper(string url, WWWForm form)
+    {
+        www = new WWW(url, form);
+        delayStartTime = Time.realtimeSinceStartup;
+    }
 
-	public WWWWrapper(string url, byte[] postData)
-	{
-		www = new WWW(url, postData);
-		delayStartTime = Time.realtimeSinceStartup;
-	}
+    public WWWWrapper(string url, byte[] postData)
+    {
+        www = new WWW(url, postData);
+        delayStartTime = Time.realtimeSinceStartup;
+    }
 
-	public WWWWrapper(string url, byte[] postData, Dictionary<string, string> headers)
-	{
-		www = new WWW(url, postData, headers);
-		delayStartTime = Time.realtimeSinceStartup;
-	}
+    public WWWWrapper(string url, byte[] postData, Dictionary<string, string> headers)
+    {
+        www = new WWW(url, postData, headers);
+        delayStartTime = Time.realtimeSinceStartup;
+    }
 
-	public void Dispose()
-	{
-		www.Dispose();
-	}
+    public void Dispose()
+    {
+        www.Dispose();
+    }
 
     public override bool keepWaiting
     {
@@ -85,72 +85,72 @@ public class WWWWrapper : CustomYieldInstruction, IDisposable
     }
 
     public string text
-	{
-		get
-		{
-			if (forceError)
-			{
-				return string.Empty;
-			}
-			else
-			{
-				return www.text;
-			}
-		}
-	}
+    {
+        get
+        {
+            if (forceError)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return www.text;
+            }
+        }
+    }
 
-	public string error
-	{
-		get
-		{
-			if (forceError)
-			{
-				return string.Format(Error[(int)errorPlatform], url);
-			}
-			else
-			{
-				return www.error;
-			}
-		}
-	}
+    public string error
+    {
+        get
+        {
+            if (forceError)
+            {
+                return string.Format(Error[(int)errorPlatform], url);
+            }
+            else
+            {
+                return www.error;
+            }
+        }
+    }
 
-	public bool isDone
-	{
-		get
-		{
-			if (delay > 0.0f)
-			{
-				return Time.realtimeSinceStartup - delayStartTime >= delay;
-			}
-			else
-			{
-				return www.isDone;
-			}
-		}
-	}
+    public bool isDone
+    {
+        get
+        {
+            if (delay > 0.0f)
+            {
+                return Time.realtimeSinceStartup - delayStartTime >= delay;
+            }
+            else
+            {
+                return www.isDone;
+            }
+        }
+    }
 
-	public float progress
-	{
-		get { return www.progress; }
-	}
+    public float progress
+    {
+        get { return www.progress; }
+    }
 
-	public string url
-	{
-		get { return www.url; }
-	}
+    public string url
+    {
+        get { return www.url; }
+    }
 
-	public Dictionary<string, string> responseHeaders
-	{
-		get
-		{
-			if (forceError)
-			{
-				return new Dictionary<string, string>();
-			}
-			else
-			{
-				return www.responseHeaders;
-			}
-		}
-	}
+    public Dictionary<string, string> responseHeaders
+    {
+        get
+        {
+            if (forceError)
+            {
+                return new Dictionary<string, string>();
+            }
+            else
+            {
+                return www.responseHeaders;
+            }
+        }
+    }
 }
--- Assets/WWWWrapperTest.cs
+++ Assets/WWWWrapperTest.cs
@@ -34,10 +34,4 @@ public class WWWWrapperTest : MonoBehaviour
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
